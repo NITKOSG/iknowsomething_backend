0c30d31c163faa3c87302965ce524712411ba859
war-turtle <kartikyadav71199@gmail.com>
2018-11-20 18:36:38 +0530

    master merged

--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,10 @@
 import auth from './routes/auth/auth';
 import user from './routes/user/user';
 import blog from './routes/blog/blog';
-import auth from './routes/auth';
 
 import ValidAuthToken from './global/middlewares/ValidAuthToken';
 
 const router = (app) => {
-  app.use('/auth', auth);
   app.use('/users', user);
   app.use('/blog', blog);
   app.use('/auth', auth);
