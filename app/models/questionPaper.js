bce661fc8417235d1eee5fa47f488f35953005b6
war-turtle <kartikyadav71199@gmail.com>
2018-12-15 15:59:35 +0530

    Question Paper Schema added

---
+++ app/models/questionPaper.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const QuestionPaperSchema = mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  semester: {
+    type: Number,
+    required: true,
+  },
+  branch: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model('QuestionPaper', QuestionPaperSchema);
